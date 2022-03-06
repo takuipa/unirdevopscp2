0e46d09cda1eeface82f0a24207e4390726acd9d
takuipa <takuipa@gmail.com>
2022-03-06 14:11:36 +0100

    nfs

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts.local -l ini-01-configuracion.yaml
-ansible-playbook -i hosts.local -l nfs-01-configuracion.yaml
+ansible-playbook -i hosts.local -l ini-01-configuration.yaml
+ansible-playbook -i hosts.local -l nfs-01-configuration.yaml
 ansible-playbook -i hosts.local -l k8s-01-master.yaml
 ansible-playbook -i hosts.local -l k8s-02-worker.yaml
 ansible-playbook -i hosts.local -l k8s-03-ingress.yaml
