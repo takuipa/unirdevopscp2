8ff552773dff3fccfe0d8ee3780469753742e087
takuipa <takuipa@gmail.com>
2022-03-11 00:10:51 +0100

    Terraform 5Vm

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,11 +1,11 @@
 variable "vms" {
   type = list(string)
   description = "Máquinas virtuales a crear"
-  default = ["master", "worker01", "worker02", "nfs"] 
+  default = ["master", "worker01", "nfs"] 
 }
 
 variable "vm_size" {
   type = list(string)
   description = "Tamaño de la máquina virtual"
-  default = ["Standard_D2_v2", "Standard_D1_v2", "Standard_D1_v2", "Standard_D1_v2"] 
+  default = ["Standard_D2_v2", "Standard_D1_v2", "Standard_D1_v2"] 
 }
