86b90fdc5d4dd271d5a6bb0a304edb357c5f19b3
takuipa <takuipa@gmail.com>
2022-03-12 20:16:58 +0100

    Add worker cluster

--- terraform/security.tf
+++ terraform/security.tf
@@ -20,6 +20,18 @@ resource "azurerm_network_security_group" "mySecGroup" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "MYSQL"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "31234"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
     tags = {
         environment = "CP2"
     }
