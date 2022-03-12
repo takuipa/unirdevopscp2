7c7bfb8ecba8324110dfe6214f54156808f0b64e
takuipa <takuipa@gmail.com>
2022-03-12 22:38:22 +0100

    Deploy app mysql

--- terraform/security.tf
+++ terraform/security.tf
@@ -32,6 +32,18 @@ resource "azurerm_network_security_group" "mySecGroup" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "HAPRXY"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3306"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
     tags = {
         environment = "CP2"
     }
