2df692c8d0a73c42fe70ac16fd8bf475af2b047f
takuipa <takuipa@gmail.com>
2022-03-12 14:57:32 +0100

    Add worker cluster

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -5,8 +5,7 @@ ansible-playbook -i hosts 01-ini-configuration.yaml
 ansible-playbook -i hosts 02-nfs-configuration.yaml
 ansible-playbook -i hosts 03-k8s-cluster-configuration.yaml
 ansible-playbook -i hosts 04-k8s-master-configuration.yaml
-# recuperar token para add nodes al cluster
 ansible-playbook -i hosts 05-k8s-cluster-usuario.yaml
 ansible-playbook -i hosts 06-k8s-master-sdn.yaml
 ansible-playbook -i hosts 07-k8s-master-ingress.yaml
-ansible-playbook -i hosts 08-k8s-worker-configuration.yaml
+ansible-playbook -i hosts 08-k8s-cluster-add-worker.yaml
